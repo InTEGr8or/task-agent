c7b6ead2e5380f1df50891d2ec072971d86075b3
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2026-03-10 13:42:47 -0700

    chore: add pre-commit hooks and fix lint issues

--- src/taskagent/models/issue.py
+++ src/taskagent/models/issue.py
@@ -1,8 +1,9 @@
-from typing import List, Optional
+from typing import List
 from pydantic import BaseModel
 
 # USV Delimiter
-USV_DELIM = '\x1f'
+USV_DELIM = "\x1f"
+
 
 class Issue(BaseModel):
     slug: str
